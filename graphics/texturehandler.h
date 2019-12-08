57bf2f53fc925f8b00e9f9068cd5592364a2375d
Guillem-Maxime <max.guillem@hotmail.fr>
2019-12-08 23:31:28 +0100

    beginning of texture handling

--- graphics/texturehandler.h
+++ graphics/texturehandler.h
@@ -4,14 +4,21 @@
 
 #include "openglobjecthandler.h"
 
+struct TextureInfos
+{
+    
+};
+
 class TextureHandler : public OpenGLObjectHandler
 {
 public:
-	
+	explicit TextureHandler() = default;
+    
+
+
 protected:
 	virtual void GenerateGLObjectId() override;
 
 private:
 	GLuint m_TextureId{0};
-
 };
